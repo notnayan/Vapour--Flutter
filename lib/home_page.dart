c2b0e07ca5cebf7792cae91235212bce019c46da
notnayan <nayanrajkhanalz@gmail.com>
2023-07-24 12:23:53 +0545

    added homepage

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    String name = "Shailu";
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Catalog App"),
+      ),
+        body: Center(
+          child: Text("Hi $name"),
+        ),
+      );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:catalog_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -9,14 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: const Text("Hi"),
-          ),
-        ),
-      ),
-    );
+
+    return const MaterialApp(
+      home: HomePage());
   }
 }
