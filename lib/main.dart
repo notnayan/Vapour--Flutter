b0cdffbf813ddaed6128c5f1b10773e182433cab
notnayan <nayanrajkhanalz@gmail.com>
2023-07-24 20:07:00 +0545

    Decorated loginpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:catalog_app/pages/home_page.dart';
 import 'package:catalog_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(){
   runApp(const MyApp());
@@ -13,6 +14,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        fontFamily: GoogleFonts.poppins().fontFamily
+      ),
       routes: {
         "/": (context)=> const LoginPage(),
         "/home":(context) => const HomePage(),
